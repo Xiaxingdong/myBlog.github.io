3e3f6656d6a88978ee590c7dd492fd81b4fda3c1
xxd <humble1030hs@gmail.com>
2020-01-21 11:22:31 +0800

    Site updated: 2020-01-21 11:22:30

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 21 2020 02:30:58 GMT+0800 (GMT+08:00)
-!function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
+// build time:Tue Jan 21 2020 11:22:23 GMT+0800 (GMT+08:00)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
